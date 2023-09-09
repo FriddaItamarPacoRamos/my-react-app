3188f9b7875a576fc842cfb0831aa175b89015e6
FRIDDAPACO <141264010+FriddaItamarPacoRamos@users.noreply.github.com>
2023-09-09 20:05:58 +0000

    salchipapa

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -11,12 +11,17 @@ export default function Home() {
       <a href="/flex"> Go to Flex </a>
       <a href="/styled"> Go to Styled </a>
 
-      <p>Home</p>
+      <p className="perro">Home</p>
       
-      <div className="box" > Box </div>
+      
+      <div className="box perro" > Box </div>
+      <div className="box2"> Quiero una</div>
+      <div className="box3"> salchipapa</div>
+      <div className="box4"> salchipapa</div>
 
-      <img
-        src={'https://imagizer.imageshack.com/img538/9889/xlMaV6.jpg'}
+
+      <img className="sl"
+        src={'https://st4.depositphotos.com/9050074/20821/i/450/depositphotos_208215132-stock-photo-homemade-peruvian-salchipapa-fries-with.jpg'}
         className="imageclass"
       /> 
 
